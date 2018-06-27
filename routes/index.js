bee27e12d7cb5458a77e14a0ed97c1184e21d8c3
stone <674656681@qq.com>
2018-06-27 11:37:07 +0800

    1
    Signed-off-by: stone <674656681@qq.com>

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,29 @@ var router = express.Router();
 var MySql = require("../sql/index.js")
 var _SQL = require("../sql/sql.js")
 const TABLE_NAME = 'member';
+const Canvas = require('canvas');
+const echarts = require('echarts');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
+  const canvas = new Canvas(parseInt(size.width,10), parseInt(size.height,10));
+  const ctx = canvas.getContext('2d');
+  ctx.font = '12px';
+  echarts.setCanvasCreator(function () {
+    return canvas;
   });
+  const chart = echarts.init(canvas);
+  options.animation = false;
+  options.textStyle = { 
+    fontSize: 12,
+  };
+  
+  chart.setOption(options);
+  res.json(chart.getOption())
+  // res.render('index', {
+  //   title: 'Express'
+  // });
+  
 });
 router.get('/member', function (req, res, next) {
   const SQL = 'SELECT * FROM member;'
