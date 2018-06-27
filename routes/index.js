b56ac65683829bbb4cc6891dfbe9b0b8b472b1ed
stone <674656681@qq.com>
2018-06-27 14:11:46 +0800

    1
    Signed-off-by: stone <674656681@qq.com>

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,19 @@ const echarts = require('echarts');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  var option =  {
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line'
+    }]
+}
   const canvas = new Canvas(500,500);
   const ctx = canvas.getContext('2d');
   ctx.font = '12px';
@@ -21,8 +34,8 @@ router.get('/', function (req, res, next) {
   };
   
   chart.setOption(options);
-  res.json({a:1})
-  // res.json(chart.getOption())
+  // res.json({a:1})
+  res.json(chart.getOption())
   // res.render('index', {
   //   title: 'Express'
   // });
